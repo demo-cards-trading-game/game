002633a4679930f26b113a5397bfd11e8a680e51
whoeverxd <joshuamoises1995@gmail.com>
2015-08-18 00:11:09 -0400

    a ver si funca

---
+++ demo/Card.java
@@ -0,0 +1,20 @@
+package demo;
+ 
+public class Card
+{
+
+  private int id;
+  
+  Card(int id)/*constructor*/
+  {
+    this.id=id;
+   }
+  
+  public int Getid()
+  {
+    return(this.id);
+  }
+
+
+
+}
