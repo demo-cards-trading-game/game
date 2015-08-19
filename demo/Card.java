7d6bfa172c65cc634eaa031f22262a2cee97d27b
whoeverxd <joshuamoises1995@gmail.com>
2015-08-18 20:59:15 -0400

    clase carta

--- demo/Card.java
+++ demo/Card.java
@@ -1,20 +1,79 @@
 package demo;
- 
+import java.util.Scanner;
 public class Card
 {
 
-  private int id;
+  private int SetNumber; /*id de la carta*/
+  private int Cost;    /*costo de mana */
+  private String Name; /*nombre de la carta*/
+  private String Class; /*clase de la carta */
+  private int Limit;   /*el limite de esa carta que puede haber en un deck*/
+  private String Source; /*el elemento de la carta */
+  /*template el fondo gay de la carta ...depende del tipo de carta*/
+  
   
   Card(int id)/*constructor*/
   {
-    this.id=id;
+    this.SetNumber=id;
    }
-  
+  /**********************funciones de consulta****************************/
   public int Getid()
   {
-    return(this.id);
+    return(this.SetNumber);
+  }
+  public int GetCost()
+  {
+    return(this.Cost);
+  }
+  
+  public String GetName()
+  {
+    return(this.Name);
   }
 
-
-
+  public String GetSource()
+  {
+  return (this.Source);
+  }
+  
+  public String Getclass()
+  {
+    return (this.Class);
+  }
+  
+  public int GetLimit()
+  {
+  return (this.Limit);
+  }
+  
+    /**********************modificadores****************************/
+  public void SetId(int New)
+  {
+    this.SetNumber=New;
+  }
+ 
+  public void SetCost(int New)
+  {
+    this.Cost=New;
+  }
+  
+  public void SetName(String New)
+  {
+    this.Name=New;
+  } 
+    
+  public void SetSource(String New)
+  {
+    this.Source=New;
+  } 
+   
+  public void SetClass(String New)
+  {
+    this.Class=New;
+  }   
+  
+  public void SetLimit(int New)
+  {
+    this.Limit=New;
+  } 
 }
