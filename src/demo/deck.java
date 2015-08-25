0cc5c2bd659667d62acdf63c72e84c513393feb5
luis <luiscarvajal2693@gmail.com>
2015-08-24 21:06:20 -0430

    variable de ejemplo

--- src/demo/deck.java
+++ src/demo/deck.java
@@ -62,7 +62,7 @@ public class deck{
  //shuffle
 public void shuffle(Card[] cards) {
 	 int n= cards.length;
-	 
+	 int aux;
 	 for(int i=0;i<n;i++){
 		 int r= i+(int)(Math.random()*(n-i)); //entre i y n-1
 		 exch(cards,i,r);
