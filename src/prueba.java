7de68f04302f8c3e31632d375cb95b7f0a4b2f99
whoeverxd <joshuamoises1995@gmail.com>
2015-08-24 22:35:46 -0400

    prueba ahora funciona con cartas de verdad

--- src/prueba.java
+++ src/prueba.java
@@ -1,36 +1,33 @@
 import demo.Card;
 import demo.deck;
 
+import java.io.IOException;
+
+import data.LoadData;
+
 
 
 public class prueba {
   public static void main(String[] args)
   {
-   /*Card c1=new Card();
-   c1.SetId("1");
-   c1.SetClass("siren");
-   c1.SetName("Waqus, the Plentiful");
-   c1.SetCost(6);
-   c1.SetLimit(4);
-   c1.SetSource("water");
-   c1.PrintCard();
-   */
-   Card[] c1 = new Card[5];
-   for(int i =0; i<5; i++){
-	   c1[i]=new Card();
-	   c1[i].SetId(""+(i+1));
-	   c1[i].SetClass("siren");
-	   c1[i].SetName("Waqus, the Plentiful");
-	   c1[i].SetCost(6);
-	   c1[i].SetLimit(4);
-	   c1[i].SetSource("water");
-	 
-   }
+  LoadData x=null;
+  
+  try {
+	x=new LoadData();
+} catch (IOException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+  
    
-   deck d = new deck(c1, 5);
-   for(int i =0; i<5; i++){
-	   //System.out.println(c1[i].Getid());
-	   System.out.println((d.extraerR()).Getid());
+   
+   deck d = new deck();
+   for(int i =1; i<5; i++){
+	   d.insertar(LoadData.Data.Consultar(i));
    }
+   for(int i =1; i<5; i++)
+	   d.extraerR().PrintCard();
+   
+   
   }
 }
