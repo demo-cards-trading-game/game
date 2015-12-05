9d01eb7c4901b11911383730d684e99a7302a18f
whoeverxd <joshuamoises1995@gmail.com>
2015-12-05 12:54:14 -0400

    push

---
+++ src/demo/PlayerGui.java
@@ -0,0 +1,43 @@
+package demo;
+
+import javax.swing.JPanel;
+
+public class PlayerGui extends JPanel {
+	public HandGui hand;
+	public DeckGui pdeck;
+	public Drained powers;
+	public Barriers barriers;
+	public fieldGui field;
+	/**
+	 * Create the panel.
+	 */
+	public PlayerGui(int x , int y, String name) 
+	{
+		/************graficos del panel*****************/
+		setBounds(x,y, 1024, 768);
+		setOpaque(false);
+		setLayout(null);
+		
+		/*inicializacion de los atributos*/
+		hand= new HandGui (0,0);
+		field = new fieldGui(220,350);
+		hand.setLocation(179, 510);
+		powers=new Drained(15,320);
+		pdeck = new DeckGui(0,0);
+		pdeck.setSize(250, 343);
+		pdeck.setLocation(770, 361);
+		barriers =new Barriers(179,500);
+		
+		/************añade atributos***********/
+		this.add(pdeck);
+		add(powers);
+		this.add(hand);
+		this.add(field);
+		add(barriers);
+		
+		
+	}
+
+	
+	
+}
