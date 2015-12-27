df78666142c5c647d7c0431bbe0dd9de440dd9f6
shadow <shadow@DESKTOP-1FK2QTO>
2015-12-27 00:43:35 -0430

    ewr

--- src/demo/Drained.java
+++ src/demo/Drained.java
@@ -2,6 +2,7 @@ package demo;
 
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JSeparator;
 import javax.swing.JLabel;
@@ -15,6 +16,7 @@ import javax.swing.border.MatteBorder;
 import java.awt.GridLayout;
 import java.awt.CardLayout;
 import javax.swing.BoxLayout;
+import javax.swing.JScrollBar;
 
 public class Drained extends JLayeredPane {
 
@@ -32,11 +34,20 @@ public class Drained extends JLayeredPane {
 		setBounds(x,y,150,384);
 		setLayout(null);
 		
-		panel=new JPanel();
-		panel.setBounds(10, 29, 130, 71);
-		add(panel);
-		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-		panel.setOpaque(false);
+		panel=new JPanel(null);
+		panel.setBounds(0, 29, 500, 50);
+		
+		
+		panel.setOpaque(true);
+		 JScrollPane scrollPane = new JScrollPane(panel);
+		 
+		 
+		
+	        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+	        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+	        scrollPane.setBounds(0, 30, 75, 37);
+	        add(scrollPane);
+	     
 		JLabel lblUndrained = new JLabel("UNDRAINED");
 		lblUndrained.setFont(new Font("Comic Sans MS", Font.BOLD, 11));
 		lblUndrained.setForeground(Color.BLACK);
@@ -63,7 +74,7 @@ public class Drained extends JLayeredPane {
 		undrained[currentundrained] = new JPanel();
 		undrained[currentundrained].setBorder(new MatteBorder(4, 3, 1, 3, (Color) new Color(0, 0, 0)));
 		undrained[currentundrained].setBackground(new Color(51, 51, 204));
-		undrained[currentundrained].setBounds(104, 325, 36, 37);
+		undrained[currentundrained].setBounds(currentundrained*40, 0, 36, 37);
 		panel.add(undrained[currentundrained]);
 		currentundrained++;
 		setVisible(true);
---
+++ src/demo/Playgui2.java
@@ -0,0 +1,14 @@
+package demo;
+
+import javax.swing.JPanel;
+
+public class Playgui2 extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Playgui2() {
+
+	}
+
+}
